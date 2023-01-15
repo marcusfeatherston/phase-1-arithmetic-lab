789ab6a5d43e91c7860e9e90e90f8c13a67c4b2e
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-14 19:15:32 -0700

    did math

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 8;
+let num2 = Number.parseFloat("7.75");
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10);
+
+let num3 = 20;
+let num4 = 16;
+
+const mod = num3 % num4;
+
+let numSet = [1, 3, 5, 20];
+
+const max = Math.max(...numSet);
+console.log(max);
